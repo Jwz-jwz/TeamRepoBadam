046e8ffb623db19815d0c7aca2c419a2041e8e68
Жавзандулам Санжаасүрэн <24LP4023@G401-CC0100.local>
2024-09-25 12:27:07 +0800

    front-end-visible-duusav

--- team-one/front-end/src/app/page.jsx
+++ team-one/front-end/src/app/page.jsx
@@ -1,3 +1,10 @@
+import LoginPage from "./components/log-in-page/LoginPage";
+import SignUp from "./sign-up-page/page";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
 }

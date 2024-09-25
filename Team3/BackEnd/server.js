c72a662ef7be31f854263d0f3a10978027e5fda1
Your Name <24LP6379@G401-CC0102.local>
2024-09-25 13:20:08 +0800

    change

--- Team3/BackEnd/server.js
+++ Team3/BackEnd/server.js
@@ -6,18 +6,43 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const users = [
+  {
+    id: 1,
+    name: "Boloro1",
+    password: "Anypass1",
+    email: "b1@nest.edu.mn",
+  },
+  {
+    id: 2,
+    name: "Boloro2",
+    password: "Anypass2",
+    email: "b2@nest.edu.mn",
+  },
+  {
+    id: 3,
+    name: "Boloro3",
+    password: "Anypass3",
+    email: "b3@nest.edu.mn",
+  },
+];
+
 app.get("/", (request, response) => {
   response.send("Hello, Your GET request has arrived.");
 });
 
-app.post("/", (request, response) => {
+app.post("/sign-in", (request, response) => {
   const { name, password } = request.body;
 
   const registeredUser = users.filter(
     (user) => user.name === name && user.password === password
   );
 
-  response.send("Hello, Your POST request has arrived.");
+  if (registeredUser.length > 0) {
+    response.send("Successfully singed in");
+  } else {
+    response.send("User not found.");
+  }
 });
 
 app.listen(port, () => {

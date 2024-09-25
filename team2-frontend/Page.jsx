5af9eba037a3ccc0ca61b276d1e27cd58d590817
Badamaa312 <badamaa312@gmail.com>
2024-09-25 12:10:03 +0800

    frontend init

---
+++ team2-frontend/Page.jsx
@@ -0,0 +1,11 @@
+export const Home = () => {
+  return (
+    <main className="pb-8 pt-8">
+      <div className="w-full flex justify-around relative">
+        <div className="container flex justify-center items-center px-8 gap-4">
+          test
+        </div>
+      </div>
+    </main>
+  );
+};
---
+++ team2-frontend/onboard/OnBoard.jsx
@@ -0,0 +1,13 @@
+export const OnBoard = () => {
+  return (
+    <main className="pb-8 pt-8">
+      <div className="w-full flex justify-around relative">
+        <div className="container flex justify-center items-center px-8 gap-4">
+
+test
+
+        </div>
+      </div>
+    </main>
+  );
+};

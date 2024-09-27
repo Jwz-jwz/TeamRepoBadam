3cc57bb0930364b19fd15a687d162644bb46b444
Badamaa312 <badamaa312@gmail.com>
2024-09-27 23:23:40 +0800

    CRUD FE finish , started BE (#44)

--- Team-2/project-crud/back-end/server.js
+++ Team-2/project-crud/back-end/server.js
@@ -1,87 +1,53 @@
 import express from "express";
 import bodyParser from "body-parser";
-import cors from "cors";
 import fs from "fs";
-import { error } from "console";
+import cors from "cors";
 
-const port = 8888;
+const port = 7777;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
-
 app.get("/", (request, response) => {
-  response.send("Hello GET huselt irlee");
+  response.send("Hello tanii get huselt irlee");
 });
 
-// app.post("/sign-in", (request, response) => {
-//   const { name, password } = request.body;
+app.post("/", (request, response) => {
+  const { name, angilal, price } = request.body;
+  fs.readFile("./data/user.json", "utf-8", (readError, data) => {
+    let savedData = data ? JSON.parse(data) : [];
 
-//   fs.readFile("./data/user.json", "utf-8", (readError, data) => {
-//     if (readError) {
-//       response.json({
-//         success: false,
-//         error: error,
-//       });
-//     }
+    if (readError) {
+      response.json({
+        success: false,
+        error: error,
+      });
+    }
 
-//     let savedData = data ? JSON.parse(data) : [];
+    const newUser = {
+      id: Date.now().toString(),
+      name: name,
+      angilal: angilal,
+      price: price,
+    };
+    savedData.push(newUser);
 
-//     const registeredUser = savedData.filter(
-//       (user) => user.name === name && user.password === password
-//     );
-//     if (registeredUser.length > 0) {
-//       response.json({
-//         success: true,
-//         user: registeredUser[0],
-//       });
-//     } else {
-//       response.json({
-//         success: false,
-//       });
-//     }
-//   });
-// });
-
-// app.post("/sign-up", (request, response) => {
-//   const { name, email, password } = request.body;
-
-//   fs.readFile("./data/user.json", "utf-8", (readError, data) => {
-//     let savedData = data ? JSON.parse(data) : [];
-
-//     if (readError) {
-//       response.json({
-//         success: false,
-//         error: error,
-//       });
-//     }
-
-//     console.log(data);
-
-//     const newUser = {
-//       id: Date.now().toString(),
-//       name: name,
-//       email: email,
-//       password: password,
-//     };
-//     savedData.push(newUser);
-
-//     fs.writeFile("./data/user.json", JSON.stringify(savedData), (error) => {
-//       if (error) {
-//         response.json({
-//           success: false,
-//           error: error,
-//         });
-//       } else {
-//         response.json({
-//           success: true,
-//           user: newUser,
-//         });
-//       }
-//     });
-//   });
-// });
+    fs.writeFile("./data/user.json", JSON.stringify(savedData), (error) => {
+      if (error) {
+        response.json({
+          success: false,
+          error: error,
+        });
+      } else {
+        response.json({
+          success: true,
+          user: newUser,
+        });
+      }
+    });
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Server ajillaj bn http://localhost:${port}`);
+  console.log(`server ajillaj ehelle http://localhost:${port}`);
 });

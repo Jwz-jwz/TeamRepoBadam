5d383b00a96e5f0ac849c1809ac49d0af55364b6
Your Name <24LP6379@G401-CC0102.local>
2024-09-30 13:07:46 +0800

    frontend boloro

--- Team3/project-2/frontend/src/app/components/Header.jsx
+++ Team3/project-2/frontend/src/app/components/Header.jsx
@@ -1,3 +1,13 @@
 export const Header = () => {
-  return <div>fjnn</div>;
+  return (
+    <div className=" flex justify-center p-4 bg-stone-800 gap-5">
+      <div className="flex justify-around items-center w-[800px] h-[80px] bg-white rounded-2xl">
+        <div>ds</div>
+        <div>
+          <input type="text" className="bg-stone-500 rounded-2xl" />
+        </div>
+        <button>Add new product</button>
+      </div>
+    </div>
+  );
 };
